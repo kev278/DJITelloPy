f598982bd3f9ae20fb545ed66b758616678c018c
Keval <kevalshah278@gmail.com>
2023-03-21 18:40:45 -0400

    chnages made to save image data

--- videoTransfer/serverZMQ.py
+++ videoTransfer/serverZMQ.py
@@ -2,8 +2,10 @@
 import cv2
 import imagezmq
 image_hub = imagezmq.ImageHub()
+#filename = 'image.png'
 while True:  # show streamed images until Ctrl-C
     rpi_name, image = image_hub.recv_image()
+    #cv2.imwrite(filename, image)
     cv2.imshow(rpi_name, image) # 1 window for each RPi
     cv2.waitKey(1)
     image_hub.send_reply(b'OK')
