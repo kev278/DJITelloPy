9e9117c0fd827c5735906f376171823765f2c296
Keval <kevalshah278@gmail.com>
2023-03-21 18:38:06 -0400

    time delay added for photo feed

--- videoTransfer/clientZMQ.py
+++ videoTransfer/clientZMQ.py
@@ -15,5 +15,6 @@ sender = imagezmq.ImageSender(connect_to='tcp://192.168.0.117:5555')
 rpi_name = socket.gethostname() # send RPi hostname with each image
 time.sleep(2.0)  # allow camera sensor to warm up
 while True:  # send images as stream until Ctrl-C
+   time.sleep(5)
    image = drone.get_frame_read().frame
    sender.send_image(rpi_name, image)
